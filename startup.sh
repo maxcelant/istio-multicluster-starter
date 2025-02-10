6b530b83623a55b65ed86c3ad2ddfec8f65d8337
Max Celant <massimiliano.celant@aa.com>
2025-02-10 15:27:20 -0600

    Updating startup script order

--- startup.sh
+++ startup.sh
@@ -14,23 +14,12 @@ both_context() {
 kind create cluster --config "cluster/cluster-east.yaml" --name east-cluster || echo "Cluster already created."
 kind create cluster --config "cluster/cluster-west.yaml" --name west-cluster || echo "Cluster already created."
 
+# Apply metallb
 both_context "kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/main/config/manifests/metallb-native.yaml"
 both_context "kubectl rollout status deploy -n metallb-system controller"
 both_context "kubectl apply -f metallb/{REGION}-lb.yaml"
-both_context "kubectl create namespace cert-manager"
-both_context "kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/${CERTMANAGER_VERSION}/cert-manager.yaml"
-both_context "kubectl create namespace istio-system"
-both_context "kubectl label namespace istio-system topology.istio.io/network='{REGION}-network'"
-both_context "kubectl apply -f cert-manager/vault-token-secret.yaml"
-both_context "istioctl install -y -f controlplanes/cluster-{REGION}.yaml"
-both_context "kubectl create namespace istioinaction"
-both_context "kubectl label namespace istioinaction istio-injection=enabled"
-both_context "kubectl -n istioinaction apply -f apps/webapp-deployment-svc.yaml"
-both_context "kubectl -n istioinaction apply -f apps/webapp-gw-vs.yaml"
-both_context "kubectl -n istioinaction apply -f apps/catalog-svc.yaml"
-kubectl -n istioinaction --context="kind-east-cluster" apply -f apps/catalog.yaml
-both_context "kubectl -n default apply -f apps/sleep.yaml"
 
+# Start vault server
 (
   cd vault
   docker-compose up -d
@@ -38,15 +27,38 @@ both_context "kubectl -n default apply -f apps/sleep.yaml"
 )
 
 both_context "kubectl create configmap coredns --from-file=cluster/coredns.yaml -n kube-system --save-config --dry-run=client -o yaml | kubectl apply -f -"
+
+both_context "kubectl create namespace cert-manager"
+both_context "kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/${CERTMANAGER_VERSION}/cert-manager.yaml"
+both_context "kubectl rollout status deploy -n cert-manager cert-manager-webhook"
+both_context "kubectl rollout status deploy -n cert-manager cert-manager-cainjector"
+both_context "kubectl rollout status deploy -n cert-manager cert-manager"
+
+both_context "kubectl create namespace istio-system"
+both_context "kubectl label namespace istio-system topology.istio.io/network='{REGION}-network'"
+both_context "kubectl apply -f cert-manager/vault-token-secret.yaml"
+
 both_context "kubectl apply -f cert-manager/issuer-{REGION}.yaml"
+both_context "kubectl apply -n istio-system -f cert-manager/certificate.yaml"
+
+both_context "istioctl install -y -f controlplanes/cluster-{REGION}.yaml"
+both_context "istioctl install -y -f gateways/{REGION}-gw.yaml"
+both_context "kubectl apply -n istio-system -f gateways/expose-services.yaml"
 
 WEST_CLUSTER_ID=$(docker ps --filter "name=west-cluster" --format "{{.ID}}")
 EAST_CLUSTER_ID=$(docker ps --filter "name=east-cluster" --format "{{.ID}}")
 WEST_CLUSTER_IP=$(docker inspect $WEST_CLUSTER_ID | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
 EAST_CLUSTER_IP=$(docker inspect $EAST_CLUSTER_ID | jq -r '.[0].NetworkSettings.Networks.kind.IPAddress')
+
+# Connect clusters
 istioctl create-remote-secret --name="east-cluster" --context="kind-east-cluster" --server="https://${EAST_CLUSTER_IP}:6443" | kubectl --context="kind-west-cluster" apply -f -
 istioctl create-remote-secret --name="west-cluster" --context="kind-west-cluster" --server="https://${WEST_CLUSTER_IP}:6443" | kubectl --context="kind-east-cluster" apply -f -
-both_context "istioctl install -y -f gateways/{REGION}-gw.yaml"
-both_context "kubectl apply -n istio-system -f cert-manager/certificate.yaml"
-both_context "kubectl apply -n istio-system -f gateways/expose-services.yaml"
 
+# Deploy dummy apps
+both_context "kubectl create namespace istioinaction"
+both_context "kubectl label namespace istioinaction istio-injection=enabled"
+both_context "kubectl -n istioinaction apply -f apps/webapp-deployment-svc.yaml"
+both_context "kubectl -n istioinaction apply -f apps/webapp-gw-vs.yaml"
+both_context "kubectl -n istioinaction apply -f apps/catalog-svc.yaml"
+kubectl -n istioinaction --context="kind-east-cluster" apply -f apps/catalog.yaml
+both_context "kubectl -n default apply -f apps/sleep.yaml"
